e5ee96ae9a906a69e82d98edd98692f8693a8ba7
Padhma <padhmasahithya@gmail.com>
2019-04-28 23:44:04 +0530

    Feat: Added Day15 files

---
+++ Day 15/Linked List.py
@@ -0,0 +1,30 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+class Solution: 
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def insert(self,head,data): 
+        if head is None:
+            head = Node(data)
+        elif head.next is None:
+            head.next = Node(data)
+        else:
+            last = head
+            while(last.next):
+                last = last.next
+            last.next = Node(data)     
+        return head
+
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)    
+mylist.display(head); 	  
