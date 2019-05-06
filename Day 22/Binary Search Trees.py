254765118e1d1c10c3cf40a7a2d6bbcf7808f1b4
Padhma <padhmasahithya@gmail.com>
2019-05-06 23:02:05 +0530

    Feat: Added Day22 files

---
+++ Day 22/Binary Search Trees.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self,data):
+        self.right=self.left=None
+        self.data = data
+class Solution:
+    def insert(self,root,data):
+        if root==None:
+            return Node(data)
+        else:
+            if data<=root.data:
+                cur=self.insert(root.left,data)
+                root.left=cur
+            else:
+                cur=self.insert(root.right,data)
+                root.right=cur
+        return root
+
+    def getHeight(self,root):
+        if root is None:
+            return -1
+        else:
+            rightDepth = self.getHeight(root.right)
+            leftDepth = self.getHeight(root.left)
+
+            if (rightDepth > leftDepth):
+                return rightDepth+1
+            else:
+                return leftDepth+1
+        
+ 
+T=int(input())
+myTree=Solution()
+root=None
+for i in range(T):
+    data=int(input())
+    root=myTree.insert(root,data)
+height=myTree.getHeight(root)
+print(height)       
