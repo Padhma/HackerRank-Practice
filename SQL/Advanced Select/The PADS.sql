19472e7989916deb8d0b22f41b1127f1406dbb97
Padhma <padhmasahithya@gmail.com>
2019-05-10 20:42:14 +0530

    Feat: Added advanced select queries I

---
+++ SQL/Advanced Select/The PADS.sql
@@ -0,0 +1,9 @@
+select concat(name,concat('(',left(occupation,1),')'))
+from occupations
+order by name asc
+                          
+select concat('There are a total of ',count(occupation),' ',lower(occupation),'s.') 
+from occupations
+group by occupation
+order by count(occupation) asc
+
