2e1e9ca225856367dd524d5aaf116b7374850668
Padhma M <padhmasahithya@gmail.com>
2020-06-09 22:33:16 +0530

    Fix: Removed Duplicates

--- Practice-Problems/Python/Maximize It!.py
+++
@@ -1,7 +0,0 @@
-from itertools import product
-k, m = map(int,input().split())
-l = ([int(x) for x in input().split()][1:] for _ in range(k))
-# for _ in range(k):
-#     l.append(set([int(x) for x in input().split()][1:]))
-result = map(lambda x: sum(i**2 for i in x) % m, product(*l))
-print(max(result))
