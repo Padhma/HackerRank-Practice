54fe436013955f81a9892772bd590428073737a0
Padhma M <padhmasahithya@gmail.com>
2020-07-15 21:32:07 +0530

    Fix: Added Built-Ins->Easy

--- Practice-Problems/Python/Athlete Sort.py
+++
@@ -1,7 +0,0 @@
-n, m = map(int, input().split())
-table = []
-for _ in range(n):
-    table.append(list(map(int,input().split())))
-k = int(input())
-for t in sorted(table, key = lambda x:x[k]):
-    print(" ".join(map(str,t)))
--- Practice-Problems/Python/ginortS.py
+++
@@ -1,1 +0,0 @@
-print("".join(sorted(input(), key = lambda x: (x.isdigit(), x.isdigit() and int(x)%2==0, x.isupper(), x.islower(), x))))
