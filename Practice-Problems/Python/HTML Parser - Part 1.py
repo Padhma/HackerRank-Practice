a849b7e1696c6a2bbaac2e8b7dfa36df245e7c47
Padhma M <padhmasahithya@gmail.com>
2020-09-18 22:06:41 +0530

    Feat: Added HTML Parser - Part 1

---
+++ Practice-Problems/Python/HTML Parser - Part 1.py
@@ -0,0 +1,21 @@
+from html.parser import HTMLParser
+
+# create a subclass and override the handler methods
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print("Start :", tag) 
+        for element in attrs:
+            print('->',element[0],'>',element[1])
+    def handle_endtag(self, tag):
+        print("End   :", tag) 
+    def handle_startendtag(self, tag, attrs):
+        print("Empty :", tag) 
+        for element in attrs:
+            print('->',element[0],'>',element[1])
+
+# instantiate the parser and fed it some HTML
+string = ""
+for _ in range(int(input())):
+    string += input()
+parser = MyHTMLParser()
+parser.feed(string)
