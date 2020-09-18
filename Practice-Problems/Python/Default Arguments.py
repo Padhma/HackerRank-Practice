16710e6a965dfb6672ef7052a9119df1b3a82933
Padhma M <padhmasahithya@gmail.com>
2020-09-18 23:07:34 +0530

    Feat: Added Default Arguments

---
+++ Practice-Problems/Python/Default Arguments.py
@@ -0,0 +1,35 @@
+class EvenStream(object):
+    def __init__(self):
+        self.current = 0
+
+    def get_next(self):
+        to_return = self.current
+        self.current += 2
+        return to_return
+
+class OddStream(object):
+    def __init__(self):
+        self.current = 1
+
+    def get_next(self):
+        to_return = self.current
+        self.current += 2
+        return to_return
+
+def print_from_stream(n, stream=None):
+    if stream is None:
+        stream = EvenStream()
+# Python’s default arguments are evaluated once when the function is defined, not each time the function is called.
+# This means that if you use a mutable default argument and mutate it, you will and have mutated that object for all future calls to the function as well.
+    for _ in range(n):
+        print(stream.get_next())
+
+
+queries = int(input())
+for _ in range(queries):
+    stream_name, n = input().split()
+    n = int(n)
+    if stream_name == "even":
+        print_from_stream(n)
+    else:
+        print_from_stream(n, OddStream())
